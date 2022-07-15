c3faa3df82c91f7b59eaa8d67aa2cec476afbb54
Martin Trat <trat@fzi.de>
2022-07-15 20:03:15 +0200

    Add __init__.py

---
+++ cdcstream/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for cdcstream."""
+
+__author__ = """Martin Trat"""
+__email__ = 'trat@fzi.de'
+__version__ = '0.1.0'
+
+from .cdcstream import CDCStream
+from .dilca_wrapper import DilcaDistance
