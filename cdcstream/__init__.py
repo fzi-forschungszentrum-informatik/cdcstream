d91459e76c241431bffd08338eee309d2445032c
Martin Trat <trat@fzi.de>
2022-07-16 19:04:57 +0200

    Increase version

--- cdcstream/__init__.py
+++ cdcstream/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Martin Trat"""
 __email__ = 'trat@fzi.de'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .cdcstream import DriftDetector, CDCStream
 from .dilca_wrapper import DilcaDistance
